ae72196da949fe7eea760db41e98cadff7aeb0eb
Jz3-3693 <jilsjilspjose@gmail.com>
2025-02-22 15:51:16 +0530

    Initial commit

--- new_app/admin.py
+++ new_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from.models import trap
+admin.site.register(trap)
 # Register your models here.
--- new_app/models.py
+++ new_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+class trap(models.Model):
+ 
+ place = models.CharField(max_length=255)
+ weather =models.CharField(max_length=255) 
+ time = models.DateTimeField()
+ # Create your models here.
